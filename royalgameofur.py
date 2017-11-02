6b3ffdc35add8aadf6c8c51aca6a1be39220226b
Ahemmetter <andreas.hemmetter@gmail.com>
2017-11-02 15:55:18 +0300

    Update 0.2
    Game should play normally now.
    Small bug appears sometimes near the end of the game, the status will say the move is not possible and doesn't let the user click.

--- royalgameofur.py
+++ royalgameofur.py
@@ -12,16 +12,16 @@ bf = []                                     # black fishki list
 wf = []                                     # white fishki list
 bpos = [-1, -1, -1, -1, -1, -1, -1]         # positions of black fishki
 wpos = [-1, -1, -1, -1, -1, -1, -1]         # positions of white fishki
-rosettes = [0, 2, 10, 14, 16]
-stone = 0
-clicked = 0
-steps = 0
-whiteturn = True
-wdone = 0
+rosettes = [0, 2, 10, 14, 16]               # red squares, on which you can move again
+stone = 0                                   # ID of selected fishka
+clicked = 0                                 # ID of clicked fishka
+steps = 0                                   # rolled number with dice
+whiteturn = True                            # keeps track of whose turn it is
+wdone = 0                                   # number of fishki that are finished already
 bdone = 0
-color = "b"
+color = "b"                                 # AI will play black (b) or white (w)
 
-bluec = "#374D7D"                           # colors
+bluec = "#374D7D"                           # flat colors
 brownc = "#8F7260"
 redc = "#C43730"
 blackc = "#2B2B2B"
@@ -168,9 +168,9 @@ class Game:
                 stone = int(item[0])
                 self.show_moves(bpos, wpos, squares)
             else:
-                print "out of range"
+                self.message.set("Out of bounds")
         else:
-            print "not clickable"
+            self.message.set("Not clickable")
 
     def click(self):
         value = 0
@@ -178,7 +178,6 @@ class Game:
         return value
 
     def show_moves(self, bpos, wpos, squares):
-        """Get item from clicking here"""
         self.redraw_board()
         if not whiteturn and (stone >= 21) and (stone <= 27):     # black
             black_goal = bpath[bpath.index(bpos[stone - 21]) + steps]
@@ -186,37 +185,69 @@ class Game:
                 if (black_goal in wpos):
                     if black_goal not in rosettes:
                         board.itemconfig(squares[black_goal], fill=lightbluec)
+                        self.click()
                 else:
                     if black_goal in rosettes:
                         board.itemconfig(squares[black_goal], fill=lightredc)
+                        self.click()
                     else:
                         board.itemconfig(squares[black_goal], fill=lightbluec)
+                        self.click()
             elif black_goal == 99:
                 board.itemconfig(bf[stone-21], fill=yellowc, outline=yellowc)
+                global bdone
+                bdone += 1
+                mes = "Black has already " + str(bdone) + " pieces home!"
+                self.message.set(mes)
+                board.coords(bf[stone - 21], bdone * 32 + 320, 16, bdone * 32 + fsize + 320, 16 + fsize)
+                bpos[stone - 21] = 99
+                self.redraw_board()
+                self.startbutton.config(state="normal")
+                self.find_winner()
+                self.change_turn()
+                # self.ai3()
+                # board.tag_unbind("clickable", "<ButtonPress-1>")
+                self.click()
+                return bpos, wpos
             else:
                 print "not allowed (already occupied or wrong square)"
 
-        if whiteturn and stone >= 28 and stone <= 34:     # white
+        elif whiteturn and stone >= 28 and stone <= 34:     # white
             white_goal = wpath[wpath.index(wpos[stone - 28]) + steps]
             if (white_goal != 99) and (white_goal not in wpos):
                 if (white_goal in bpos):
                     if white_goal not in rosettes:
                         board.itemconfig(squares[white_goal], fill=lightbluec)
+                        self.click()
                 else:
                     if white_goal in rosettes:
                         board.itemconfig(squares[white_goal], fill=lightredc)
+                        self.click()
                     else:
                         board.itemconfig(squares[white_goal], fill=lightbluec)
+                        self.click()
             elif white_goal == 99:
                 board.itemconfig(wf[stone-28], fill=yellowc, outline=yellowc)
+                global wdone
+                wdone += 1
+                mes = "White has already " + str(wdone) + " pieces home!"
+                self.message.set(mes)
+                board.coords(wf[stone - 28], wdone * 32 + 320, 272, wdone * 32 + fsize + 320, 272 + fsize)
+                wpos[stone - 28] = 99
+                self.redraw_board()
+                self.startbutton.config(state="normal")
+                self.find_winner()
+                self.change_turn()
+                # board.tag_unbind("clickable", "<ButtonPress-1>")
+                self.ai3()
+                return bpos, wpos
+                # self.click()
             else:
-                print "not allowed (already occupied or wrong square)"
-        self.click()
+                self.message.set("Not allowed")
+
 
     def move(self):
         x, y = self.find_position(clicked - 1)
-        print "clicked = " + str(clicked)
-        print "stone = " + str(stone)
         if stone >= 21 and stone <= 27:     # black
             if bpath.index(bpos[stone - 21]) + steps <= 17:
                 black_goal = bpath[bpath.index(bpos[stone - 21]) + steps]
@@ -227,23 +258,22 @@ class Game:
                         # and is not a rosette..
                         if (black_goal not in rosettes):
                             # then move there, kick it
-                            # kick stone
                             board.coords(wf[wpos.index(black_goal)], wpos.index(black_goal) * 32 + 64, 272, wpos.index(black_goal) * 32 + fsize + 64, 272 + fsize)
                             wpos[wpos.index(black_goal)] = -1
                             board.coords(bf[stone - 21], x, y, x + fsize, y + fsize)
                             bpos[stone - 21] = clicked - 1
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
                             self.change_turn()
-                            self.ai3(color)
                             board.tag_unbind("clickable", "<ButtonPress-1>")
                             return bpos, wpos
                         # and is a rosette..
                         else:
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
                             self.change_turn()
-                            self.ai3(color)
                             return bpos, wpos
                     else:
                         if (black_goal in rosettes):
@@ -251,36 +281,26 @@ class Game:
                             board.coords(bf[stone - 21], x, y, x + fsize, y + fsize)
                             bpos[stone - 21] = clicked - 1
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
-                            board.tag_unbind("clickable", "<ButtonPress-1>")
+                            self.ai3()
+
                             return bpos, wpos
                         else:
                             # move like always
                             board.coords(bf[stone - 21], x, y, x + fsize, y + fsize)
                             bpos[stone - 21] = clicked - 1
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
                             self.change_turn()
-                            self.ai3(color)
                             board.tag_unbind("clickable", "<ButtonPress-1>")
                             return bpos, wpos
-                elif black_goal == 99:
-                    global bdone
-                    bdone += 1
-                    print "bdone = " + str(bdone)
-                    board.coords(bf[stone - 21], bdone * 32 + 320, 16, bdone * 32 + fsize + 320, 16 + fsize)
-                    bpos[stone - 21] = 99
-                    self.redraw_board()
-                    self.startbutton.config(state="normal")
-                    self.find_winner()
-                    self.change_turn()
-                    self.ai3(color)
-                    board.tag_unbind("clickable", "<ButtonPress-1>")
-                    return bpos, wpos
                 else:
-                    print "You can't move there (black)"
+                    self.message.set("You can't move there1")
             else:
-                print "Not possible"
+                self.message.set("Not allowed")
+                self.change_turn()
 
         if stone >= 28 and stone <= 34:     # white
             if wpath.index(wpos[stone - 28]) + steps <= 17:
@@ -292,23 +312,23 @@ class Game:
                         # and is not a rosette..
                         if (white_goal not in rosettes):
                             # then move there, kick it
-                            # kick stone
                             board.coords(bf[bpos.index(white_goal)], bpos.index(white_goal) * 32 + 64, 16, bpos.index(white_goal) * 32 + fsize + 64, 16 + fsize)
                             bpos[bpos.index(white_goal)] = -1
                             board.coords(wf[stone - 28], x, y, x + fsize, y + fsize)
                             wpos[stone - 28] = clicked - 1
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
                             self.change_turn()
                             board.tag_unbind("clickable", "<ButtonPress-1>")
-                            self.ai3(color)
                             return bpos, wpos
                         # and is a rosette..
                         else:
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
                             self.change_turn()
-                            self.ai3(color)
+                            # board.tag_unbind("clickable", "<ButtonPress-1>")
                             return bpos, wpos
                     else:
                         if (white_goal in rosettes):
@@ -316,48 +336,36 @@ class Game:
                             board.coords(wf[stone - 28], x, y, x + fsize, y + fsize)
                             wpos[stone - 28] = clicked - 1
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
-                            board.tag_unbind("clickable", "<ButtonPress-1>")
-                            self.ai3(color)
+                            # board.tag_unbind("clickable", "<ButtonPress-1>")
                             return bpos, wpos
                         else:
                             # move like always
                             board.coords(wf[stone - 28], x, y, x + fsize, y + fsize)
                             wpos[stone - 28] = clicked - 1
                             self.redraw_board()
+                            self.rolled.set("")
                             self.startbutton.config(state="normal")
                             self.change_turn()
                             board.tag_unbind("clickable", "<ButtonPress-1>")
-                            self.ai3(color)
                             return bpos, wpos
-                elif white_goal == 99:
-                    global wdone
-                    wdone += 1
-                    print "wdone = " + str(wdone)
-                    board.coords(wf[stone - 28], wdone * 32 + 320, 272, wdone * 32 + fsize + 320, 272 + fsize)
-                    wpos[stone - 28] = 99
-                    self.redraw_board()
-                    self.startbutton.config(state="normal")
-                    self.find_winner()
-                    self.change_turn()
-                    board.tag_unbind("clickable", "<ButtonPress-1>")
-                    self.ai3(color)
-                    return bpos, wpos
                 else:
-                    print "You can't move there (black)"
+                    self.message.set("You can't move there2")
             else:
-                print "Not possible"
+                self.message.set("Not allowed")
+                self.change_turn()
 
     def change_turn(self):
         global whiteturn
         if whiteturn:
             whiteturn = False
             self.message.set("Black's turn")
-            self.ai3(color)
+            self.ai3()
         else:
             whiteturn = True
             self.message.set("White's turn")
-            self.ai3(color)
+            self.startbutton.config(state="normal")
 
     def find_winner(self):
         if wdone == 7:
@@ -367,56 +375,67 @@ class Game:
             tkMessageBox.showinfo("We have a winner!", "Black wins!")
             root.quit()
 
-    def ai3(self, color):
+    def ai3(self):
         if color == "b":
             if not whiteturn:
                 self.throw_dice()
-                found = 0
-                found_stone = 0
-                for i in range(0, 7):
-                    # kick someone
-                    if bpath.index(bpos[i]) + steps <= 17:
-                        black_goal = bpath[bpath.index(bpos[i]) + steps]
-                        if (black_goal in wpos) and (black_goal not in rosettes) and (black_goal not in bpos):
-                            found = black_goal
-                            found_stone = i + 21
-                            break
-                    else:
-                        print "Not possible"
-                if not found:
+                found = -99
+                found_stone = -99
+                if steps != 0:
                     for i in range(0, 7):
-                        # get rosette
+                        # kick someone
                         if bpath.index(bpos[i]) + steps <= 17:
                             black_goal = bpath[bpath.index(bpos[i]) + steps]
-                            if (black_goal not in wpos) and (black_goal in rosettes) and (black_goal not in bpos):
+                            if (black_goal in wpos) and (black_goal not in rosettes) and (black_goal not in bpos):
                                 found = black_goal
                                 found_stone = i + 21
-                                print found
                                 break
-                        else:
-                            print "Not possible"
-                    if not found:
+                    if found == -99:
                         for i in range(0, 7):
-                            # other square
-                            if bpath.index(bpos[i]) + steps <= 17:
-                                black_goal = bpath[bpath.index(bpos[i]) + steps]
-                                if (black_goal not in wpos) and (black_goal not in rosettes) and (black_goal not in bpos):
-                                    found = black_goal
-                                    found_stone = i + 21
-                                    break
-                            else:
-                                print "not possible"
-                if found:
+                            # finish piece
+                            if bpath.index(bpos[i]) + steps == 17:
+                                global bdone
+                                bdone += 1
+                                board.coords(bf[i], bdone * 32 + 320, 16, bdone * 32 + fsize + 320, 16 + fsize)
+                                bpos[i] = 99
+                                found = 99
+                                found_stone = i + 21
+                                board.itemconfig(bf[i], fill=yellowc, outline=yellowc)
+                                self.redraw_board()
+                                self.startbutton.config(state="normal")
+                                self.find_winner()
+                                self.change_turn()
+                                board.tag_unbind("clickable", "<ButtonPress-1>")
+                                return bpos, wpos
+                                break
+                        if found == -99:
+                            for i in range(0, 7):
+                                # get rosette
+                                if bpath.index(bpos[i]) + steps <= 17:
+                                    black_goal = bpath[bpath.index(bpos[i]) + steps]
+                                    if (black_goal not in wpos) and (black_goal in rosettes) and (black_goal not in bpos):
+                                        found = black_goal
+                                        found_stone = i + 21
+                                        break
+                            if found == -99:
+                                for i in range(0, 7):
+                                    # other square
+                                    if bpath.index(bpos[i]) + steps <= 17:
+                                        black_goal = bpath[bpath.index(bpos[i]) + steps]
+                                        if (black_goal not in wpos) and (black_goal not in rosettes) and (black_goal not in bpos):
+                                            found = black_goal
+                                            found_stone = i + 21
+                                            break
+                if found != -99 and found != 99:
                     global clicked
                     clicked = found + 1
-                    print clicked
                     global stone
                     stone = found_stone
-                    self.move()
+                    # self.move()
+                    root.after(800, self.move)
                 else:
-                    print "hz"
+                    self.message.set("Skip move")
                     self.change_turn()
-
 
 root = Tk()  # makes empty window
 
